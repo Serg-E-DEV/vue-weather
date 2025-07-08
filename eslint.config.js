350b79950b6da025f059cfbadb87a4a3ee893267
Sergey E <sibharakter@gmail.com>
2025-07-08 14:39:02 +0700

    config

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import vue from 'eslint-plugin-vue';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+
+export default [
+    js.configs.recommended,
+    ...vue.configs['flat/recommended'],
+    prettier,
+    {
+        files: ['**/*.vue', '**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            'vue/multi-word-component-names': 'off',
+            'vue/require-default-prop': 'off',
+            'no-unused-vars': 'error',
+        },
+    },
+    {
+        files: ['vite.config.*', 'eslint.config.*', '*.cjs', '*.mjs'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+                exports: 'readonly',
+                process: 'readonly',
+            },
+        },
+    },
+];
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const a = 0;
+const b = '<img />';
+const color = 'red';
+// const id = 'blockTest';
+// const isRed = false;
+// const message = 'Строка сообщения';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <main>Hello world! {{ a }}, {{ isRed ? 'true' : 'false' }}</main>
+    <div v-html="b"></div>
+    <div :class="color">Цвет</div>
+    <div :class="`color-${color}`">Цвет</div>
+    <div class="section">Секция</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.red {
+    background: red;
 }
 </style>
